6183386df83af0a6f020eca1ac5306962a563345
Akarsh vashishth <Akarshvash041218@gmail.com>
2025-09-19 14:12:02 +0530

    Arithmetic operators

---
+++ week1.1.py
@@ -0,0 +1,11 @@
+#Types of operators in Python
+# 1) Arithmetic operators (+ , - , * , / , % , // , **)
+
+print("Addition 5 + 7 =" , 5+7 , "(It's also used for Concatination)") 
+print("Subtraction 5 - 7 =", 5-7 )
+print("Multiplication 5 * 7 = ", 5 * 7)
+print("Division 5 / 7 =" , 5 / 7 , "(It will always return float value)")
+print("Floor division 5 // 7 =" , 5 //7 , "(It always returns int values)")
+print("Modulus 5 % 7 =", 5 % 7 ,"(Always returns the remainder in division)")
+print("Modulus 7 % 5 =" , 7 % 5 )
+print("Exponentiation 5 ** 7 =" ,  5**7 , "(5 to the power of 7)" )
